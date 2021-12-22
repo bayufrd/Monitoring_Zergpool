2a05ba7a131f873d1f63321a5a67ba0f8f4b473a
bayufrd <55101246+bayufrd@users.noreply.github.com>
2021-12-22 22:42:01 +0700

    Update stat.sh

--- stat.sh
+++ stat.sh
@@ -24,46 +24,63 @@ echo -e "${g}        /                                       "; $r
 MSG_FILE="bayuM.msg"
 JSON_FILE="bayuM.json"
 DATE=$(date +"%d-%m-%Y %H:%M:%S")
+WEB="http://api.zergpool.com:8080/api/walletEx?address=$Wallet"
+
 loop=0
 
 while [ $loop = 0 ]
 
 do
-echo -e "Wallet Monitoring~ \nCurrency\t: $coin" > $MSG_FILE
-echo -e "Date : " $DATE"\n" >> $MSG_FILE
 
 
-WEB="http://api.zergpool.com:8080/api/walletEx?address=$Wallet"
-
-#json init
+#=============json init================================#
 echo -e "${g}Json initialization"; $r
 json_init=$(curl -s "$WEB" | jq -r '.')
 echo -e "$json_init" > $JSON_FILE
 echo -e "${g}===================="; $r
 
 
-#wrapp
+#==============wrapp====================================#
 unsold=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"unsold": ')
 balance=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"balance": ')
 unpaid=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"unpaid": ')
 paid24h=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"paid24h": ')
 minpay=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"minpay": ')
-minpay_sunday=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"minpay_sunday": ')
+minpay_sunday=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"minpay_sunday":                                                                                 ')
 total=$(cat "$JSON_FILE" | awk -F, 'NR>1{print $1}' RS='"total": ')
-eimi=$(cat "$JSON_FILE" | jq -r ' .miners[] | "ID: " + (.ID + " | Hashrate: " + .hashrate)')
+eimi=$(cat "$JSON_FILE" | jq -r ' .miners[] | "ID: " + (.ID + " | Hashrate: " +                                                                                 .hashrate)')
 
 count=$(cat "$JSON_FILE" | jq -r '.miners | length')
 
 #total
-cat "$JSON_FILE" | jq -r '[ .miners[] | select(.hashrate | contains(" Mh/s") ) ]' >> mhs.json
-mhs=$(cat "mhs.json" | jq -r '[.[].hashrate | gsub("Mh/s";"") | tonumber] | add')
+cat "$JSON_FILE" | jq -r '[ .miners[] | select(.hashrate | contains(" Mh/s") ) ]                                                                                ' >> mhs.json
+mhs=$(cat "mhs.json" | jq -r '[.[].hashrate | gsub("Mh/s";"") | tonumber] | add'                                                                                )
 
-cat "$JSON_FILE" | jq -r '[ .miners[] | select(.hashrate | contains(" kh/s") ) ]' >> khs.json
-khs=$(cat "khs.json" | jq -r '[.[].hashrate | gsub("kh/s";"") | tonumber] | add | floor/1000')
+cat "$JSON_FILE" | jq -r '[ .miners[] | select(.hashrate | contains(" kh/s") ) ]                                                                                ' >> khs.json
+khs=$(cat "khs.json" | jq -r '[.[].hashrate | gsub("kh/s";"") | tonumber] | add                                                                                 | floor/1000')
 
 
 hstot=$(echo "$mhs + $khs" | bc)
-#print
+#================CHECK===================================#
+
+if [ -z "$total" ]
+then
+    #fail
+          sudo find khs.json -delete
+          sudo find mhs.json -delete
+          sudo find bayuM.json -delete
+          sudo find bayuM.msg -delete
+
+    echo -e "${g}FAIL Connection Problem.. try again"; $r
+
+else
+    #send tele
+    loop=1
+#==================PRINT==================================#
+
+echo -e "${g}Sending report.."; $r
+echo -e "Wallet Monitoring~ \nCurrency\t: $coin" > $MSG_FILE
+echo -e "Date : " $DATE"\n" >> $MSG_FILE
 echo -e "${g}Generate report.."; $r
 echo -e "Immature\t: "$unsold $coin >> $MSG_FILE
 echo -e "Balance\t\t: "$balance $coin >> $MSG_FILE
@@ -77,16 +94,6 @@ sed -i "s/ID: /\n\t\tID: /g" $MSG_FILE
 echo -e "Total Online\t: "$count" / 20 Miners" >> $MSG_FILE
 echo -e "Hashrate Total\t: "$hstot " Mh/s" >> $MSG_FILE
 echo -e "${g}===================="; $r
-
-if [ -z "$total" ]
-then
-    #fail
-    echo -e "${g}FAIL Connection Problem.. try again"; $r
-
-else
-    #send tele
-    loop=1
-    echo -e "${g}Sending report.."; $r
     MESSAGE=`cat $MSG_FILE`
     URL="https://api.telegram.org/bot$TOKEN/sendMessage"
     for ID in "${CHAT_ID[@]}"
@@ -94,11 +101,11 @@ else
         curl -X POST -so /dev/null $URL -d chat_id=$ID -d text="$MESSAGE";
         echo -e "${g}===================="; $r
         echo -e "${g}SUCCESS.. DONE."; $r
-        cat $MSG_FILE; $r
-        find khs.json -delete
-        find mhs.json -delete
-        find bayuM.json -delete
-        find bayuM.msg -delete
+
+        sudo find khs.json -delete
+        sudo find mhs.json -delete
+        sudo find bayuM.json -delete
+        sudo find bayuM.msg -delete
     done
 fi
 done
